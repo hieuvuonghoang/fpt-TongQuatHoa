6fdd10f661cf9ba885fb9916200e9d4a72bfc30e
Hiếu Vương Hoàng <vuonghoanghieu@outlook.com.vn>
2019-05-31 11:42:11 +0700

    Update DichNhaP

--- ArcPy/DichNhaP/DichNhaP.py
+++ ArcPy/DichNhaP/DichNhaP.py
@@ -14,7 +14,7 @@ class DichNhaP:
         self.fCDoanTimDuongBo = "DoanTimDuongBo"
         self.distanceDoanTimDuongBoMeter = 20.0
         self.distanceDoanTimDuongBo = str(self.distanceDoanTimDuongBoMeter) + " Meters"
-        self.radiusMoveNhaPMaxMeter = 21.0
+        self.radiusMoveNhaPMaxMeter = 50.0
         self.radiusMoveNhaPMax = str(self.radiusMoveNhaPMaxMeter) + " Meters"
         self.radiusNhaPMeter = 2.0
         self.radiusNhaP = str(self.radiusNhaPMeter) + " Meters"
@@ -26,9 +26,12 @@ class DichNhaP:
         self.CreateBufferDoanTimDuongBo()
         self.CreateFeatureNhaPCanDich()
         self.CreateBufferNhaPCanDich()
-        self.CreatePolygonContainsA()
+        self.CreatePolygonContains()
+        self.CreatePointRandomInPolygonContains()
+        self.UpdateShapeNhaPCanDichInFinal()
 
     def CopyNhaPToMemory(self):
+        print "CopyNhaPToMemory"
         self.fCNhaPInMemory = "in_memory\\NhaPInMemory"
         arcpy.AddField_management(in_table = self.pathFCNhaP,
                                   field_name = "FID_NhaP",
@@ -43,6 +46,7 @@ class DichNhaP:
                                      drop_field = ["FID_NhaP"])
 
     def CreateBufferDoanTimDuongBo(self):
+        print "CreateBufferDoanTimDuongBo"
         # Create Buffer DoanTimDuongBo
         self.bufferDoanTimDuongBo = "in_memory\\BufferDoanTimDuongBo"
         arcpy.Buffer_analysis(in_features = self.pathDoanTimDuongBo,
@@ -54,6 +58,7 @@ class DichNhaP:
                                           out_layer = self.bufferDoanTimDuongBoLayer)
 
     def CreateFeatureNhaPCanDich(self):
+        print "CreateFeatureNhaPCanDich"
         # Create Layer NhaP
         self.nhaPLayer = "NhaPLayer"
         arcpy.MakeFeatureLayer_management(in_features = self.fCNhaPInMemory,
@@ -74,6 +79,7 @@ class DichNhaP:
                                           out_layer = self.nhaPCanDichLayer)
 
     def CreateBufferNhaPCanDich(self):
+        print "CreateBufferNhaPCanDich"
         tableTemp = "in_memory\\TableTemp"
         arcpy.GenerateNearTable_analysis(in_features = self.fCNhaPCanDich,
                                          near_features = self.pathDoanTimDuongBo,
@@ -96,19 +102,26 @@ class DichNhaP:
         arcpy.RemoveJoin_management(in_layer_or_view = self.nhaPCanDichLayer,
                                     join_name = tableTemp.split("\\")[1])
         self.bufferNhaPCanDich = "in_memory\\BufferNhaPCanDich"
-        #self.bufferNhaPCanDich = os.path.join(self.pathDefaultGDB, "BufferNhaPCanDich")
         arcpy.Buffer_analysis(in_features = self.fCNhaPCanDich,
                               out_feature_class = self.bufferNhaPCanDich,
                               buffer_distance_or_field = "RadiusBuffer")
-        arcpy.Delete_management(self.fCNhaPCanDich)
+        #arcpy.Delete_management(self.fCNhaPCanDich)
+        pass
 
-    def CreatePolygonContainsA(self):
-        polygonTempA = "in_memory\\PolygonTempA"
+    def CreatePolygonContains(self):
+        print "CreatePolygonContains"
+        polygonTempA = os.path.join(os.path.join(self.pathProcessGDB, self.fDDanCuCoSoHaTang), "PolygonTempA")
         arcpy.Erase_analysis(in_features = self.bufferNhaPCanDich,
                              erase_features = self.bufferDoanTimDuongBo,
                              out_feature_class = polygonTempA)
-        polygonTempB = os.path.join(os.path.join(self.pathDefaultGDB, self.fDDanCuCoSoHaTang), "PolygonTempB")
-        arcpy.MultipartToSinglepart_management(in_features = polygonTempA,
+        polygonTempALayer = "PolygonTempALayer"
+        arcpy.MakeFeatureLayer_management(in_features = polygonTempA,
+                                          out_layer = polygonTempALayer)
+        arcpy.SelectLayerByAttribute_management(in_layer_or_view = polygonTempALayer,
+                                                selection_type = "NEW_SELECTION",
+                                                where_clause = "Shape_Area <> 0")
+        polygonTempB = os.path.join(os.path.join(self.pathProcessGDB, self.fDDanCuCoSoHaTang), "PolygonTempB")
+        arcpy.MultipartToSinglepart_management(in_features = polygonTempALayer,
                                                out_feature_class = polygonTempB)
         polygonTempBLayer = "PolygonTempBLayer"
         arcpy.MakeFeatureLayer_management(in_features = polygonTempB,
@@ -139,116 +152,62 @@ class DichNhaP:
         arcpy.SelectLayerByAttribute_management(in_layer_or_view = polygonTempBLayer,
                                                 selection_type = "NEW_SELECTION",
                                                 where_clause = sqlQueryErasePolygon)
+        self.resultPolygonContains = os.path.join(self.pathDefaultGDB, "ResultPolygonContains")
         arcpy.CopyFeatures_management(in_features = polygonTempBLayer,
-                                      out_feature_class = os.path.join(self.pathDefaultGDB, "ResultPolygonContains"))
-        arcpy.Delete_management(polygonTempB)
+                                      out_feature_class = self.resultPolygonContains)
+        self.resultPolygonContainsLayer = "ResultPolygonContainsLayer"
+        arcpy.MakeFeatureLayer_management(in_features = self.resultPolygonContains,
+                                          out_layer = self.resultPolygonContainsLayer)
+        #arcpy.Delete_management(polygonTempB)
+        #arcpy.Delete_management(polygonTempA)
+        pass
 
-    def CreatePolygonContains(self):
-        # Create Buffer DoanTimDuongBo
-        bufferDoanTimDuongBo = "in_memory\\BufferDoanTimDuongBo"
-        arcpy.Buffer_analysis(in_features = os.path.join(os.path.join(self.pathProcessGDB, self.fDGiaoThong), self.fCDoanTimDuongBo),
-                              out_feature_class = bufferDoanTimDuongBo,
-                              buffer_distance_or_field = self.distanceDoanTimDuongBo)
-        # MakeFeatureLayer outLayerBufferFCDoanTimDuongBo
-        bufferDoanTimDuongBoLayer = "BufferDoanTimDuongBoLayer"
-        arcpy.MakeFeatureLayer_management(in_features = bufferDoanTimDuongBo,
-                                          out_layer = bufferDoanTimDuongBoLayer)
-        # MakeFeatureLayer NhaP
-        nhaPLayer = "NhaPLayer"
-        arcpy.MakeFeatureLayer_management(in_features = os.path.join(os.path.join(self.pathProcessGDB, self.fDDanCuCoSoHaTang), self.fCNhaP),
-                                          out_layer = nhaPLayer)
-        # Chon NhaP can dich chuyen (NhaP nam trong bufferDoanTimDuongBo)
-        arcpy.SelectLayerByLocation_management(in_layer = nhaPLayer,
-                                               overlap_type = "WITHIN",
-                                               select_features = bufferDoanTimDuongBoLayer,
-                                               search_distance = "0 Meters",
-                                               selection_type = "NEW_SELECTION")
-        # Generate Near Table
-        outNearTable = "in_memory\\OutNearTable"
-        arcpy.GenerateNearTable_analysis(in_features = nhaPLayer,
-                                         near_features = os.path.join(os.path.join(self.pathProcessGDB, self.fDGiaoThong), self.fCDoanTimDuongBo),
-                                         out_table = outNearTable,
-                                         search_radius = self.distanceDoanTimDuongBo)
-        # Buffer NhaP can dich chuyen
-        nhaPCanDichChuyenBuffer = "in_memory\\NhaPCanDichChuyenBuffer"
-        arcpy.Buffer_analysis(in_features = nhaPLayer,
-                              out_feature_class = nhaPCanDichChuyenBuffer,
-                              buffer_distance_or_field = self.radiusMoveNhaP)
-        # Create Polygon de dich chuyen NhaP vao ben trong Polygon
-        polygonContains = "in_memory\\PolygonContains"
-        #polygonContains = os.path.join(self.pathDefaultGDB, "PolygonContains")
-        arcpy.Erase_analysis(in_features = nhaPCanDichChuyenBuffer,
-                             erase_features = bufferDoanTimDuongBoLayer,
-                             out_feature_class = polygonContains)
-        # Add Filed FID_NhaP
-        arcpy.AddField_management(in_table = polygonContains,
-                                  field_type = "Long",
-                                  field_name = "FID_NhaP")
-        arcpy.CalculateField_management(in_table = polygonContains,
-                                        field = "FID_NhaP",
-                                        expression = "!ORIG_FID!",
-                                        expression_type = "PYTHON_9.3")
-        # Doi voi moi NhaP co the co nhieu polygonContains ("ORIG_FID"), can chon polygonContains co Shape_Area lon nhat trong cac polygonContains
-        ## Convert polygonContains Multipart to Singlepart
-        polygonContainsSinglepart = "in_memory\\PolygonContainsSinglepart"
-        #polygonContainsSinglepart = os.path.join(self.pathDefaultGDB, "PolygonContainsSinglepart")
-        arcpy.MultipartToSinglepart_management(in_features = polygonContains,
-                                               out_feature_class = polygonContainsSinglepart)
-        # AddField Shape_Area
-        arcpy.AddField_management(in_table = polygonContainsSinglepart,
-                                  field_type = "Double",
-                                  field_name = "Shape_Area")
-        arcpy.CalculateField_management(in_table = polygonContainsSinglepart,
-                                        field = "Shape_Area",
-                                        expression = "!shape.geodesicArea@SQUAREMETERS!",
-                                        expression_type = "PYTHON_9.3")
-        ## Tu nhung Feature polygonContains co chung FID_NhaP chon Feature Shape_Area lon nhat
-        polygonContainsSinglepartStatistics = "in_memory\\PolygonContainsSinglepartStatistics"
-        #polygonContainsSinglepartStatistics = os.path.join(self.pathDefaultGDB, "PolygonContainsSinglepartStatistics")
-        arcpy.Statistics_analysis(in_table = polygonContainsSinglepart,
-                                  out_table = polygonContainsSinglepartStatistics,
-                                  statistics_fields = [["Shape_Area", "MAX"]],
-                                  case_field = ["FID_NhaP"])
-        outTableSelect = "in_memory\\outTableSelect"
-        arcpy.TableSelect_analysis(in_table = polygonContainsSinglepartStatistics,
-                                   out_table = outTableSelect,
-                                   where_clause = "FREQUENCY > 1")
-        arcpy.MakeFeatureLayer_management(in_features = polygonContainsSinglepart,
-                                          out_layer = "PolygonContainsSinglepartLayer")
-        arcpy.AddJoin_management(in_layer_or_view = "PolygonContainsSinglepartLayer",
-                                 in_field = "FID_NhaP",
-                                 join_table = outTableSelect,
-                                 join_field = "FID_NhaP")
-        arcpy.SelectLayerByAttribute_management(in_layer_or_view = "PolygonContainsSinglepartLayer",
-                                                selection_type = "NEW_SELECTION",
-                                                where_clause = "(MAX_Shape_Area IS NOT NULL) AND (Shape_Area <> MAX_Shape_Area)")
-        arcpy.RemoveJoin_management("PolygonContainsSinglepartLayer")
-        sqlQueryErasePolygon = ""
-        with arcpy.da.SearchCursor("PolygonContainsSinglepartLayer", ["OID@"]) as cursor:
+    def CreatePointRandomInPolygonContains(self):
+        print "CreatePointRandomInPolygonContains"
+        self.pointRandom = os.path.join(self.pathDefaultGDB, "PointRandom")
+        arcpy.CreateRandomPoints_management(out_path = self.pathDefaultGDB,
+                                            out_name = "PointRandom",
+                                            constraining_feature_class = self.resultPolygonContains,
+                                            number_of_points_or_field = "1")
+        tableATemp = "in_memory\\TableATemp"
+        arcpy.TableSelect_analysis(in_table = self.resultPolygonContains,
+                                   out_table = tableATemp)
+        arcpy.JoinField_management(in_data = self.pointRandom,
+                                   in_field = "CID",
+                                   join_table = tableATemp,
+                                   join_field = "OBJECTID",
+                                   fields = ["FID_NhaP"])
+        self.pointRandomJoin = os.path.join(self.pathDefaultGDB, "PointRandomJoin")
+        arcpy.CopyFeatures_management(in_features = self.pointRandom,
+                                      out_feature_class = self.pointRandomJoin)
+    
+    def UpdateShapeNhaPCanDichInFinal(self):
+        print "UpdateShapeNhaPCanDichInFinal"
+        self.nhaPProcessLayer = "NhaPProcessLayer"
+        arcpy.MakeFeatureLayer_management(in_features = os.path.join(os.path.join(self.pathFinalGDB, self.fDDanCuCoSoHaTang), self.fCNhaP),
+                                          out_layer = self.nhaPProcessLayer)
+        sqlQueryNhaP = ""
+        with arcpy.da.SearchCursor(self.pointRandomJoin, ["FID_NhaP"]) as cursor:
             for row in cursor:
-                sqlQueryErasePolygon += "OBJECTID <> " + str(row[0]) + " AND "
-        sqlQueryErasePolygon = sqlQueryErasePolygon[slice(len(sqlQueryErasePolygon) - 5)]
-        arcpy.SelectLayerByAttribute_management(in_layer_or_view = "PolygonContainsSinglepartLayer",
+                sqlQueryNhaP += "OBJECTID = " + str(row[0]) + " OR "
+        sqlQueryNhaP = sqlQueryNhaP[slice(len(sqlQueryNhaP) - 4)]
+        arcpy.SelectLayerByAttribute_management(in_layer_or_view = self.nhaPProcessLayer,
                                                 selection_type = "NEW_SELECTION",
-                                                where_clause = sqlQueryErasePolygon)
-        arcpy.CopyFeatures_management(in_features = "PolygonContainsSinglepartLayer",
-                                      out_feature_class = os.path.join(self.pathDefaultGDB, "ResultPolygonContains"))
-        #outPolygonTempALayer = "OutPolygonTempALayer"
-        #arcpy.MakeFeatureLayer_management(in_features = outPolygonTempA,
-        #                                  out_layer = outPolygonTempALayer)
-        ## Chon NhaP nam trong OutPolygonTempALayer
-        #arcpy.SelectLayerByLocation_management(in_layer = nhaPLayer,
-        #                                       overlap_type = "WITHIN",
-        #                                       select_features = outPolygonTempALayer,
-        #                                       search_distance = "0 Meters",
-        #                                       selection_type = "NEW_SELECTION")
-        #outBufferNhaPSelect = "in_memory\\OutBufferNhaPSelect"
-        #arcpy.Buffer_analysis(in_features = nhaPLayer,
-        #                      out_feature_class = outBufferNhaPSelect,
-        #                      buffer_distance_or_field = self.radiusNhaP)
-        #arcpy.Erase_analysis(in_features = outPolygonTempA,
-        #                     erase_features = outBufferNhaPSelect,
-        #                     out_feature_class = os.path.join(self.pathDefaultGDB, "resultPolygonContains"))
+                                                where_clause = sqlQueryNhaP)
+        with arcpy.da.UpdateCursor(self.nhaPProcessLayer, ["OID@", "Shape@"]) as cursor:
+            for row in cursor:
+                print "\tOID: " + str(row[0])
+                found = False;
+                with arcpy.da.UpdateCursor(self.pointRandomJoin, ["FID_NhaP", "Shape@"]) as cursorSub:
+                    for rowSub in cursorSub:
+                        if row[0] == rowSub[0]:
+                            found = True;
+                            row[1] = rowSub[1]
+                            cursor.updateRow(row)
+                            cursorSub.deleteRow()
+                            break
+                if found == False:
+                    cursor.deleteRow()
 
 if __name__ == '__main__':
     print "DichNhaP Tools"
