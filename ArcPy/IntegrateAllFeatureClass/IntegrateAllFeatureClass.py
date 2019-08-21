f00801ded4cfe5bc4644a3d2c400d5ce6925323f
Hiếu Vương Hoàng <vuonghoanghieu@outlook.com.vn>
2019-08-21 12:51:47 +0700

    Update Tools

--- ArcPy/IntegrateAllFeatureClass/IntegrateAllFeatureClass.py
+++ ArcPy/IntegrateAllFeatureClass/IntegrateAllFeatureClass.py
@@ -11,39 +11,109 @@ class IntegrateAllFeatureClass:
     def __init__(self):
         self.pathProcessGDB = "C:\\Generalize_25_50\\50K_Process.gdb"
         self.pathFinalGDB = "C:\\Generalize_25_50\\50K_Final.gdb"
-        self.pathFileConfig = os.path.join(os.path.dirname(os.path.realpath(__file__)), "Config.json")
+        self.pathFileConfig = os.path.join(os.path.dirname(os.path.realpath(__file__)), "ConfigSimplify.json")
+        self.pathFileConfigTopo = os.path.join(os.path.dirname(os.path.realpath(__file__)), "ConfigTopo.json")
+        self.pathFileConfigTopoCopy = os.path.join(os.path.dirname(os.path.realpath(__file__)), "ConfigTopo_Copy.json")
         self.outPutMerge = os.path.join(self.pathProcessGDB, "FeatureClassMerge")
         self.outPutSimplify = os.path.join(self.pathProcessGDB, "FeatureClassMerge_Simplify")
-        if os.path.isfile(self.pathFileConfig):
-            self.ReadFileConfig()
-        else:
-            print "Not Found: " + self.pathFileConfig + "?\n Create FileConfig..."
+        # Read File Config Simplify
+        if not os.path.isfile(self.pathFileConfig):
+            print "... Not Found: " + self.pathFileConfig + "?\n  ... Create File ConfigSimplify..."
             self.CreateFileConfig()
+        self.ReadFileConfig()
+        # Read File Config Process Topo
+        if not os.path.isfile(self.pathFileConfigTopo):
+            print "... Not Found: " + self.pathFileConfigTopo + "?\n  ... Create File ConfigTopo..."
+            self.CreateFileConfigTopo()
+        self.ReadFileConfigTopo()
         pass
 
     def Execute(self):
         # Init Workspace
         arcpy.env.overwriteOutput = True
-        self.ReadFileConfig()
 
-        # Integrate
-        arrLayerPolygon, arrLayerPolyline = self.MakeFeatureLayer()
-        self.Integrate(arrLayerPolyline, arrLayerPolygon)
-        self.Integrate(arrLayerPolygon, arrLayerPolyline)
+        ## Integrate
+        #arrLayerPolygon, arrLayerPolyline = self.MakeFeatureLayer()
+        #self.Integrate(arrLayerPolyline, arrLayerPolygon)
+        #self.Integrate(arrLayerPolygon, arrLayerPolyline)
 
-        # PolyLine
-        inFC_SimplifyAllPolyline = self.MergeFeatureClass("Polyline")
-        inFC_ExportFC = self.SimplifyAllPolyline(inFC_SimplifyAllPolyline)
-        self.ExportFeatureClassAfterSimplify("Polyline", inFC_ExportFC)
-        self.FeatureClassSimplifyToMultiPoint("Polyline")
-        self.ErasePoint("Polyline")
+        ## Clean InMemory
+        #arcpy.Delete_management(in_data = "in_memory")
 
-        # Polygon
-        inFC_SimplifyAllPolygon = self.MergeFeatureClass("Polygon")
-        inFC_ExportFC = self.SimplifyAllPolygon(inFC_SimplifyAllPolygon)
-        self.ExportFeatureClassAfterSimplify("Polygon", inFC_ExportFC)
-        self.FeatureClassSimplifyToMultiPoint("Polygon")
-        self.ErasePoint("Polygon")
+        ## PolyLine
+        #inFC_SimplifyAllPolyline = self.MergeFeatureClass("Polyline")
+        #inFC_ExportFC = self.SimplifyAllPolyline(inFC_SimplifyAllPolyline)
+        #self.ExportFeatureClassAfterSimplify("Polyline", inFC_ExportFC)
+        #self.FeatureClassSimplifyToMultiPoint("Polyline")
+        #self.ErasePoint("Polyline")
+
+        ## Clean InMemory
+        #arcpy.Delete_management(in_data = "in_memory")
+
+        ## Polygon
+        #inFC_SimplifyAllPolygon = self.MergeFeatureClass("Polygon")
+        #inFC_ExportFC = self.SimplifyAllPolygon(inFC_SimplifyAllPolygon)
+        #self.ExportFeatureClassAfterSimplify("Polygon", inFC_ExportFC)
+        #self.FeatureClassSimplifyToMultiPoint("Polygon")
+        #self.ErasePoint("Polygon")
+
+        ## Clean InMemory
+        #arcpy.Delete_management(in_data = "in_memory")
+        self.CreateFeaturePointRemoveOne()
+        pass
+
+    def CreateFeaturePointRemoveOne(self):
+        for elemConfigTopo in self.configTopoTools.listConfig:
+            featureDataSetPolyLine = elemConfigTopo.featureDataSet
+            for elemPolyline in elemConfigTopo.listPolyline:
+                featureClassPolyLine = FeatureClass(elemPolyline.featureClass)
+                for elemPolygonTopo in elemPolyline.polygonTopos:
+                    featureDataSetPolygon = elemPolygonTopo.featureDataSet
+                    for elemPolygon in elemPolygonTopo.listPolygon:
+                        featureClassPolygon = FeatureClass(elemPolygon.featureClass)
+                        if elemPolygon.processTopo == True:
+                            self.CreateFeaturePointRemoveOneSub(featureDataSetPolygon, featureClassPolygon, featureDataSetPolyLine, featureClassPolyLine)
+                            pass
+        pass
+
+    def CreateFeaturePointRemoveOneSub(self, featureDataSetPolygon, featureClassPolygon, featureDataSetPolyLine, featureClassPolyLine):
+        # Make Feature Layer
+        featureClassPolygon.SetFeatureClassSimplify()
+        inFCPolygonSimplify = os.path.join(self.pathProcessGDB, os.path.join(featureDataSetPolygon, featureClassPolygon.featureClassSimplify))
+        inFCPolygonSimplifyLayer = "inFCPolygonSimplifyLayer"
+        arcpy.MakeFeatureLayer_management(in_features = inFCPolygonSimplify,
+                                          out_layer = inFCPolygonSimplifyLayer)
+        featureClassPolyLine.SetFeatureClassPointRemove()
+        inFCPolylinePointRemove = os.path.join(self.pathProcessGDB, os.path.join(featureDataSetPolyLine, featureClassPolyLine.featureClassPointRemove))
+        inFCPolylinePointRemoveLayer = "inFCPolylinePointRemoveLayer"
+        arcpy.MakeFeatureLayer_management(in_features = inFCPolylinePointRemove,
+                                          out_layer = inFCPolylinePointRemoveLayer)
+        # Polygon To Polyline
+        outFCPolygonToPolyline = "in_memory\\outFCPolygonToPolyline"
+        arcpy.PolygonToLine_management(in_features = inFCPolygonSimplifyLayer,
+                                       out_feature_class = outFCPolygonToPolyline)
+        # Make Feature Layer
+        outFCPolygonToPolylineLayer = "outFCPolygonToPolylineLayer"
+        arcpy.MakeFeatureLayer_management(in_features = outFCPolygonToPolyline,
+                                          out_layer = outFCPolygonToPolylineLayer)
+        # Select By Location
+        arcpy.SelectLayerByLocation_management(in_layer = inFCPolylinePointRemoveLayer,
+                                               overlap_type = "WITHIN",
+                                               select_features = outFCPolygonToPolylineLayer,
+                                               search_distance = "0 Meters",
+                                               selection_type = "NEW_SELECTION")
+        # Copy Feature Class
+        outFCCopy = "in_memory\\outFCCopy"
+        arcpy.CopyFeatures_management(in_features = inFCPolylinePointRemoveLayer,
+                                      out_feature_class = outFCCopy)
+        # Erase
+        featureClassPolyLine.SetFeatureClassPointRemoveOne()
+        outErase = os.path.join(self.pathProcessGDB, os.path.join(featureDataSetPolyLine, featureClassPolyLine.featureClassPointRemoveOne))
+        arcpy.Erase_analysis(in_features = inFCPolylinePointRemove,
+                             erase_features = outFCCopy,
+                             out_feature_class = outErase)
+        # Clean InMemory
+        arcpy.Delete_management("in_memory")
         pass
 
     def ErasePoint(self, option):
@@ -331,6 +401,44 @@ class IntegrateAllFeatureClass:
         return "FID_" + featureClass, fieldType
         pass
 
+    def CreateFileConfigTopo(self):
+        arrTemp = []
+        for fcDataSetTemp in arcpy.Describe(self.pathProcessGDB).children:
+            elemConfigTopoC = ElemConfigTopoC(fcDataSetTemp.baseName)
+            for fcTemp in arcpy.Describe(fcDataSetTemp.catalogPath).children:
+                if fcTemp.featureType == "Simple" and fcTemp.shapeType == "Polygon":
+                    elemConfigTopoD = ElemConfigTopoD(fcTemp.baseName, False)
+                    elemConfigTopoC.InsertElemListPolygon(elemConfigTopoD)
+            if elemConfigTopoC.GetLength() > 0:
+                arrTemp.append(elemConfigTopoC)
+
+        configTopoTools = ConfigTopoTools()
+        for fcDataSetTemp in arcpy.Describe(self.pathProcessGDB).children:
+            elemConfigTopoA = ElemConfigTopoA(fcDataSetTemp.baseName)
+            for fcTemp in arcpy.Describe(fcDataSetTemp.catalogPath).children:
+                if fcTemp.featureType == "Simple" and fcTemp.shapeType == "Polyline":
+                    elemConfigTopoB = ElemConfigTopoB(fcTemp.baseName)
+                    for itemTemp in arrTemp:
+                        elemConfigTopoB.InsertElemListPolygon(itemTemp)
+                    if elemConfigTopoB.GetLength() > 1:
+                        elemConfigTopoA.InsertElemListPolyline(elemConfigTopoB)
+            if elemConfigTopoA.GetLength() > 1:
+                configTopoTools.InsertElemListConfig(elemConfigTopoA)
+
+        textConfig = json.dumps(obj = configTopoTools.GetDict(), indent = 4, sort_keys = True)
+        file = open(self.pathFileConfigTopo, "w")
+        file.write(textConfig)
+        file.close()
+        pass
+
+    def ReadFileConfigTopo(self):
+        self.configTopoTools = ConfigTopoTools()
+        file = open(self.pathFileConfigTopo, "r")
+        textConfig = file.read()
+        file.close()
+        self.configTopoTools.InitFromDict(json.loads(textConfig))
+        pass
+
     def CreateFileConfig(self):
         self.configTools = ConfigTools()
         for fcDataSetTemp in arcpy.Describe(self.pathProcessGDB).children:
@@ -356,6 +464,185 @@ class IntegrateAllFeatureClass:
         textConfig = file.read()
         file.close()
         self.configTools.InitFromDict(json.loads(textConfig))
+        pass
+
+class ConfigTopoTools:
+
+    def __init__(self):
+        self.listConfig = []
+
+    def InsertElemListConfig(self, elemListConfig):
+        self.listConfig.append(elemListConfig)
+
+    def GetLength(self):
+        return len(self.listConfig)
+        pass
+
+    def GetDict(self):
+        listTemp = []
+        for elemListConfig in self.listConfig:
+            listTemp.append(elemListConfig.GetDict())
+        return listTemp;
+
+    def InitFromDict(self, dataJson):
+        self.listConfig = []
+        for elemListConfigTemp in dataJson:
+            elemConfigTopoA = ElemConfigTopoA(elemListConfigTemp['featureDataSet'])
+            for elemListPolylineTemp in elemListConfigTemp['listPolyline']:
+                elemConfigTopoB = ElemConfigTopoB(elemListPolylineTemp['featureClass'])
+                for elemListPolygonTopoTemp in elemListPolylineTemp['polygonTopos']:
+                    elemConfigTopoC = ElemConfigTopoC(elemListPolygonTopoTemp['featureDataSet'])
+                    for elemListPolygonTemp in elemListPolygonTopoTemp['listPolygon']:
+                        elemConfigTopoD = ElemConfigTopoD(elemListPolygonTemp['featureClass'], elemListPolygonTemp['processTopo'])
+                        elemConfigTopoC.InsertElemListPolygon(elemConfigTopoD)
+                    elemConfigTopoB.InsertElemListPolygon(elemConfigTopoC)
+                elemConfigTopoA.InsertElemListPolyline(elemConfigTopoB)
+            self.InsertElemListConfig(elemConfigTopoA)
+
+# Elem of ConfigTopoTools.listConfig
+class ElemConfigTopoA:
+
+    def __init__(self, featureDataSet):
+        self.featureDataSet = featureDataSet
+        self.listPolyline = []
+        pass
+
+    def InsertElemListPolyline(self, elemListPolyline):
+        self.listPolyline.append(elemListPolyline)
+
+    def GetLength(self):
+        return len(self.listPolyline)
+        pass
+
+    def GetDict(self):
+        listTempPolyline = []
+        for elemList in self.listPolyline:
+            listTempPolyline.append(elemList.GetDict())
+        return {
+            "featureDataSet": self.featureDataSet,
+            "listPolyline": listTempPolyline
+        }
+
+# Elem of ElemConfigTopoA.listPolyline
+class ElemConfigTopoB:
+
+    def __init__(self, featureClass):
+        self.featureClass = featureClass
+        self.polygonTopos = []
+        pass
+
+    def InsertElemListPolygon(self, polygonTopo):
+        self.polygonTopos.append(polygonTopo)
+    
+    def GetLength(self):
+        return len(self.polygonTopos)
+        pass
+
+    def GetDict(self):
+        listTempPolygon = []
+        for elemList in self.polygonTopos:
+            listTempPolygon.append(elemList.GetDict())
+        return {
+            "featureClass": self.featureClass,
+            "polygonTopos": listTempPolygon
+        }
+
+# Elem of ElemConfigTopoB.listPolygon
+class ElemConfigTopoC:
+
+    def __init__(self, featureDataSet):
+        self.featureDataSet = featureDataSet
+        self.listPolygon = []
+        pass
+
+    def SetFeatureDataSet(self, featureDataSet):
+        self.featureDataSet = featureDataSet
+        pass
+
+    def InsertElemListPolygon(self, elemListPolygon):
+        self.listPolygon.append(elemListPolygon)
+
+    def GetLength(self):
+        return len(self.listPolygon)
+        pass
+
+    def GetDict(self):
+        listTempPolygon = []
+        for elemList in self.listPolygon:
+            listTempPolygon.append(elemList.__dict__)
+        return {
+            "featureDataSet": self.featureDataSet,
+            "listPolygon": listTempPolygon
+        }
+
+# Elem of ElemConfigTopoC.listPolygon
+class ElemConfigTopoD:
+
+    def __init__(self, featureClass, processTopo):
+        self.featureClass = featureClass
+        self.processTopo = processTopo
+        pass
+    
+    def SetFeatureClassInMemory(self):
+        self.featureClassInMemory = "in_memory\\" + self.featureClass
+        pass
+
+    def SetFeatureLayer(self):
+        self.featureLayer = self.featureClass + "Layer"
+        pass
+
+    def SetFeatureClassAllPoint(self):
+        self.featureClassAllPoint = self.featureClass + "_AllPoint"
+        pass
+
+    def SetFeatureClassSimplify(self):
+        self.featureClassSimplify = self.featureClass + "_Simplify"
+        pass
+
+    def SetFeatureClassSimplifyAllPoint(self):
+        self.featureClassSimplifyAllPoint = self.featureClass + "_Simplify_AllPoint"
+        pass
+
+    def SetFeatureClassPointRemove(self):
+        self.featureClassPointRemove = self.featureClass + "_PointRemove"
+        pass
+
+    def SetFeatureClassPointRemoveOne(self):
+        self.featureClassPointRemoveOne = self.featureClass + "_PointRemove_One"
+        pass
+
+class FeatureClass:
+
+    def __init__(self, featureClass):
+        self.featureClass = featureClass
+        pass
+    
+    def SetFeatureClassInMemory(self):
+        self.featureClassInMemory = "in_memory\\" + self.featureClass
+        pass
+
+    def SetFeatureLayer(self):
+        self.featureLayer = self.featureClass + "Layer"
+        pass
+
+    def SetFeatureClassAllPoint(self):
+        self.featureClassAllPoint = self.featureClass + "_AllPoint"
+        pass
+
+    def SetFeatureClassSimplify(self):
+        self.featureClassSimplify = self.featureClass + "_Simplify"
+        pass
+
+    def SetFeatureClassSimplifyAllPoint(self):
+        self.featureClassSimplifyAllPoint = self.featureClass + "_Simplify_AllPoint"
+        pass
+
+    def SetFeatureClassPointRemove(self):
+        self.featureClassPointRemove = self.featureClass + "_PointRemove"
+        pass
+
+    def SetFeatureClassPointRemoveOne(self):
+        self.featureClassPointRemoveOne = self.featureClass + "_PointRemove_One"
         pass
 
 class ConfigTools:
@@ -415,24 +702,35 @@ class ElemList:
     def __init__(self, featureClass, runFeatureClass):
         self.featureClass = featureClass
         self.runFeatureClass = runFeatureClass
+        pass
     
     def SetFeatureClassInMemory(self):
         self.featureClassInMemory = "in_memory\\" + self.featureClass
+        pass
 
     def SetFeatureLayer(self):
         self.featureLayer = self.featureClass + "Layer"
+        pass
 
     def SetFeatureClassAllPoint(self):
         self.featureClassAllPoint = self.featureClass + "_AllPoint"
+        pass
 
     def SetFeatureClassSimplify(self):
         self.featureClassSimplify = self.featureClass + "_Simplify"
+        pass
 
     def SetFeatureClassSimplifyAllPoint(self):
         self.featureClassSimplifyAllPoint = self.featureClass + "_Simplify_AllPoint"
+        pass
 
     def SetFeatureClassPointRemove(self):
         self.featureClassPointRemove = self.featureClass + "_PointRemove"
+        pass
+
+    def SetFeatureClassPointRemoveOne(self):
+        self.featureClassPointRemoveOne = self.featureClass + "_PointRemove_One"
+        pass
 
 class RunTime:
 
