09ba57e53759ef973df9f7f2b1a84268689f5f4e
Vuong Hoang Hieu <vuonghoanghieu96@gmail.com>
2019-09-03 10:54:45 +0700

    Update

--- ArcPy/RanhGioiBai/RanhGioiBai.py
+++ ArcPy/RanhGioiBai/RanhGioiBai.py
@@ -13,6 +13,7 @@ class RanhGioiBai:
         self.pathProcessGDB = "C:\\Generalize_25_50\\50K_Process.gdb"
         self.pathFinalGDB = "C:\\Generalize_25_50\\50K_Final.gdb"
         self.fDThuyHe = "ThuyHe"
+        self.fCBaiBoiA = "BaiBoiA"
         self.fCRanhGioiBai = "RanhGioiBai"
         self.fCDuongBoNuoc = "DuongBoNuoc"
         self.fCDuongMepNuoc = "DuongMepNuoc"
@@ -21,23 +22,44 @@ class RanhGioiBai:
     def Execute(self):
         arcpy.env.overwriteOutput = True
         arcpy.env.workspace = "C:\\Generalize_25_50\\50K_Final.gdb"
-        pathBaiBoiFinal = os.path.join(os.path.join(self.pathFinalGDB, self.fDThuyHe), self.fCRanhGioiBai)
+        pathBaiBoiFinal = os.path.join(os.path.join(self.pathFinalGDB, self.fDThuyHe), self.fCBaiBoiA)
         outPutFeatureToLine = "in_memory\\outPutFeatureToLine"
         arcpy.PolygonToLine_management(in_features = pathBaiBoiFinal,
                                        out_feature_class = outPutFeatureToLine)
         pathDuongBoNuocFinal = os.path.join(os.path.join(self.pathFinalGDB, self.fDThuyHe), self.fCDuongBoNuoc)
-        #
         outPutEraseBoNuoc = "in_memory\\outPutEraseBoNuoc"
         arcpy.Erase_analysis(in_features = outPutFeatureToLine,
                              erase_features = pathDuongBoNuocFinal,
                              out_feature_class = outPutEraseBoNuoc,
                              cluster_tolerance = "0 Meters")
+        pathDuongMepNuocFinal = os.path.join(os.path.join(self.pathFinalGDB, self.fDThuyHe), self.fCDuongMepNuoc)
         outPutEraseMepNuoc = "in_memory\\outPutEraseMepNuoc"
         arcpy.Erase_analysis(in_features = outPutEraseBoNuoc,
-                             erase_features = outPutEraseMepNuoc,
+                             erase_features = pathDuongMepNuocFinal,
                              out_feature_class = outPutEraseMepNuoc,
                              cluster_tolerance = "0 Meters")
-        
+        pathRanhGioiBaiFinal = os.path.join(os.path.join(self.pathFinalGDB, self.fDThuyHe), self.fCRanhGioiBai)
+        with arcpy.da.UpdateCursor(pathRanhGioiBaiFinal, ["OID@"]) as cursor:
+            for row in cursor:
+                cursor.deleteRow()
+        with arcpy.da.InsertCursor(pathRanhGioiBaiFinal, ["Shape@", "maNhanDang", "ngayThuNhan", "ngayCapNhat", "maDoiTuong", "nguonDuLieu", "maTrinhBay", "tenManh", "soPhienHieuManhBanDo"]) as curosrA:
+            with arcpy.da.SearchCursor(outPutEraseMepNuoc, ["Shape@", "LEFT_FID", "RIGHT_FID"]) as curosrB:
+                with arcpy.da.SearchCursor(pathBaiBoiFinal, ["OID@", "maNhanDang", "ngayThuNhan", "ngayCapNhat", "nguonDuLieu", "tenManh", "soPhienHieuManhBanDo"]) as cursorC:
+                    for rowB in curosrB:
+                        baiBoiID = None
+                        if rowB[1] != -1:
+                            baiBoiID = rowB[1]
+                            pass
+                        elif rowB[2] != -1:
+                            baiBoiID = rowB[2]
+                            pass
+                        if baiBoiID != None:
+                            cursorC.reset()
+                            for rowC in cursorC:
+                                if rowC[0] == baiBoiID:
+                                    curosrA.insertRow((rowB[0], "Auto", rowC[2], rowC[3], "LG04", rowC[4], None, rowC[5], rowC[6]))
+                                    break
+                        pass
         pass
 
 class RunTime:
