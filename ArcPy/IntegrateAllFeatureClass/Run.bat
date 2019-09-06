e9054ce67e5e59fd96209cc757b3e9431b27db51
Hiếu Vương Hoàng <vuonghoanghieu@outlook.com.vn>
2019-09-06 10:32:05 +0700

    Update File Run.bat

--- ArcPy/IntegrateAllFeatureClass/Run.bat
+++ ArcPy/IntegrateAllFeatureClass/Run.bat
@@ -1,8 +1,22 @@
 @echo off
-title IntegrateAllFeatureClass
+title Simpliify Process Topology
 
 :: Runs your command
-set pathfile=%~dp0%IntegrateAllFeatureClass.py
+set pathfile=%~dp0%InitData.py
+echo.%pathfile%
+c:
+cd %python27%
+python %pathfile%
+
+:: Runs your command
+set pathfile=%~dp0%UpdatePointRemovePolyline.py
+echo.%pathfile%
+c:
+cd %python27%
+python %pathfile%
+
+:: Runs your command
+set pathfile=%~dp0%UpdateShape.py
 echo.%pathfile%
 c:
 cd %python27%
